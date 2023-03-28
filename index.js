db3b36ace483dd05df4f3274fe7b2a6c9577c653
Naman-2001 <naman.aggarwal2001@gmail.com>
2023-03-29 05:18:41 +0530

    submitting code-Wed Mar 29 2023 05:18:39 GMT+0530 (India Standard Time)

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.get('/', (req, res) => {
 //your code here
 app.post('/add', (req, res) => {
   const {a,b} = req.body;
+  console.log()
   res.status(200).send(a+b);
+
   // res.sendFile(path.join(__dirname + '/main.html'));
 });
 module.exports = app;
